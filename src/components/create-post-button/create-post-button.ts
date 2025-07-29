f2adebfdcb980fb37d5c08c3ab7e967228b05a4f
Manuel Melgar <manuelmelgar0198@gmail.com>
2025-07-29 16:04:38 -0700

    refactor(create-post-button): Encapsula boton 'Crear Nuevo Post' en un Web Component

---
+++ src/components/create-post-button/create-post-button.ts
@@ -0,0 +1,30 @@
+import { loadComponentAsset } from '../../utils/domUtils.js';
+
+class CreatePostButton extends HTMLElement {
+    private _shadowRoot!: ShadowRoot;
+    private _initialized = false;
+
+    constructor() {
+        super();
+        this._shadowRoot = this.attachShadow({ mode: 'open' });
+    }
+
+    async connectedCallback(): Promise<void> {
+        if (this._initialized) return;
+        this._initialized = true;
+
+        await loadComponentAsset('./dist/components/create-post-button/create-post-button', 'html', this._shadowRoot, '#create-post-button-template');
+        await loadComponentAsset('./dist/components/create-post-button/create-post-button', 'css', this._shadowRoot);
+
+        this._setupEventListeners();
+    }
+
+    _setupEventListeners(): void {
+        const createPostBtn = this._shadowRoot.getElementById('createPostBtn');
+        createPostBtn?.addEventListener('click', () => {
+            document.dispatchEvent(new CustomEvent('open-post-form'));
+        });
+    }
+}
+
+customElements.define('create-post-button', CreatePostButton);
