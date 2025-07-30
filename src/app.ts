ce7d4a48681322a1aafcec8a0b09651a13d07df7
Manuel Melgar <manuelmelgar0198@gmail.com>
2025-07-30 10:31:29 -0700

    chore(app): Inicializa posts de ejemplo al cargar la aplicacion

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,9 @@
 import './components/create-post-button/create-post-button.js';
 import './components/post-form/post-form.js';
+import './components/post-list/post-list.js';
+import { seedInitialPosts } from './api/postApi.js';
+
+document.addEventListener('DOMContentLoaded', async () => {
+    console.log('Aplicación de Blog inicializando...');
+    await seedInitialPosts();
+});
